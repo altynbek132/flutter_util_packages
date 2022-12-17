cab65070a84e38b37d80c7a141dcce75c6b62be5
altynbek132 <altynbek.ktl@gmail.com>
2022-12-18 01:34:04 +0600

    ex

--- example/lib/flutter/dragscrollsheet.dart
+++ example/lib/flutter/dragscrollsheet.dart
@@ -29,63 +29,18 @@ abstract class _DragscrollsheetVmBase with Store, MobxStoreBase, LoggerMixin {
   /// PROXY --------------------------------------------------------------------
 
   /// OBSERVABLES --------------------------------------------------------------
-  final controllerRaw = DraggableScrollableController();
-  late final controller = MobxUtils.fromCN(controllerRaw).disposeWithVm(this);
 
-  @observable
-  late var minChildSize = _minChildSize;
-  final _minChildSize = 0.1;
-  @observable
-  late var maxChildSize = _maxChildSize;
-  final _maxChildSize = 1.0;
-  @observable
-  late var snap = _snap;
-  final _snap = true;
-
-  // todo Altynbek: report bugfix
-  // proxy, bugfix
-  double get initialChildSize {
-    if (!controllerRaw.isAttached) {
-      return 0.2;
-    }
-    return controllerRaw.size;
-  }
+  late final dragScrollController = DraggableScrollableControllerWrapper(
+    initialChildSizeRef: 316 / 812,
+    minChildSizeRef: 316 / 812,
+    maxChildSizeRef: 707 / 812,
+  )..disposeWithVm(this);
 
   /// COMPUTED -----------------------------------------------------------------
-  @computed
-  double get blur {
-    if (!controller.value.isAttached) {
-      return 0;
-    }
-    return controller.value.size;
-  }
 
   /// STREAM REACTION ----------------------------------------------------------
 
   /// ACTIONS ------------------------------------------------------------------
-  @action
-  Future<void> animateTo(double value) => controller.value.animateTo(value,
-      duration: Duration(milliseconds: 1000), curve: Curves.easeInOut);
-
-  @action
-  Future<void> jumpTo(double value) async => controller.value.jumpTo(value);
-
-  @action
-  Future<void> close() async {
-    // initialChildSize = controller.value.size;
-    // snap = false;
-    // await Future.delayed(const Duration(milliseconds: 1000));
-    minChildSize = 0;
-    WidgetsBinding.instance.addPostFrameCallback((_) => animateTo(0));
-  }
-
-  @action
-  Future<void> open() async {
-    await animateTo(_minChildSize);
-    // WidgetsBinding.instance
-    //     .addPostFrameCallback((_) => minChildSize = _minChildSize);
-    /*initialChildSize = */ minChildSize = _minChildSize;
-  }
 
   /// UTIL METHODS -------------------------------------------------------------
 
@@ -129,69 +84,56 @@ class _HomePageState extends State<HomePage> {
         body: Stack(
           children: [
             SizedBox.expand(
-              child: DraggableScrollableSheet(
-                controller: vm.controllerRaw,
-                snap: vm.snap,
-                initialChildSize: vm.initialChildSize,
-                minChildSize: vm.minChildSize,
-                maxChildSize: vm.maxChildSize,
-                builder:
-                    (BuildContext context, ScrollController scrollController) {
-                  return Container(
-                    color: Colors.blue[100],
-                    child: ListView.builder(
-                      controller: scrollController,
-                      itemCount: 25,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(title: Text('Item $index'));
-                      },
-                    ),
-                  );
-                },
-              ),
+              child: Observer(builder: (context) {
+                final vm = this.vm.dragScrollController;
+                return DraggableScrollableSheet(
+                  controller: vm.controllerRaw,
+                  initialChildSize: vm.initialChildSize,
+                  minChildSize: vm.minChildSize,
+                  maxChildSize: vm.maxChildSize,
+                  snap: true,
+                  builder: (BuildContext context,
+                      ScrollController scrollController) {
+                    return Container(
+                      color: Colors.blue[100],
+                      child: ListView.builder(
+                        controller: scrollController,
+                        itemCount: 25,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ListTile(title: Text('Item $index'));
+                        },
+                      ),
+                    );
+                  },
+                );
+              }),
             ),
             Column(
               children: [
                 Row(
                   children: [
                     TextButton(
-                        onPressed: () => vm.animateTo(0.5),
+                        onPressed: () => vm.dragScrollController.animateTo(0.5),
                         child: Text('vm.animateTo(0.5)')),
                     TextButton(
-                        onPressed: () => vm.animateTo(0),
+                        onPressed: () => vm.dragScrollController.animateTo(0),
                         child: Text('vm.animateTo(0)')),
                     TextButton(
-                        onPressed: () => vm.animateTo(1),
+                        onPressed: () => vm.dragScrollController.animateTo(1),
                         child: Text('vm.animateTo(1)')),
                   ],
                 ),
                 Row(
                   children: [
                     TextButton(
-                        onPressed: () => vm.close(), child: Text('vm.close()')),
+                        onPressed: () => vm.dragScrollController.close(),
+                        child: Text('vm..close()')),
                     TextButton(
-                        onPressed: () => vm.open(), child: Text('vm.open()')),
+                        onPressed: () => vm.dragScrollController.open(),
+                        child: Text('vm..open()')),
                   ],
                 ),
               ],
-            ),
-            Align(
-              alignment: Alignment.center,
-              child: ClipRRect(
-                child: Observer(builder: (context) {
-                  return ImageFiltered(
-                    imageFilter: ImageFilter.blur(
-                      sigmaX: vm.blur * 3,
-                      sigmaY: vm.blur * 3,
-                    ),
-                    child: SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: FlutterLogo(),
-                    ),
-                  );
-                }),
-              ),
             ),
           ],
         ),
