ad13a51faa9fe63f9a10cb858f778c893af0f91c
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-11 14:13:31 +0500

    [worker_method_channel] Add toString() method to Message class

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'exception.dart';
 
 class Message {
@@ -12,4 +13,9 @@ class Message {
     this.body,
     this.exception,
   });
+
+  @override
+  String toString() {
+    return 'Message(requestId: $requestId, method: $method, body: $body, exception: $exception)';
+  }
 }
