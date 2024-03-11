4cf6d9f6ea228bf65489581737113642e74a200f
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-11 14:17:24 +0500

    [worker_method_channel] 🚀 Added new export statements for worker_method_channel and src files.

---
+++ lib/src/src.dart
@@ -0,0 +1,5 @@
+export 'exception.dart';
+export 'message.dart';
+export 'web_worker_method_channel_platform_interface.dart';
+export 'worker.dart';
+export 'worker_base.dart';
--- lib/worker_method_channel.dart
+++ lib/worker_method_channel.dart
@@ -1,1 +1,3 @@
 library worker_method_channel;
+
+export 'src/src.dart';
