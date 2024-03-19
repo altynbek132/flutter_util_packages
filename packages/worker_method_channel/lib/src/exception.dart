6d85821d5162f856e1b8aa1d1dcda86c6a8e5b6e
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-19 14:42:08 +0500

    [worker_method_channel] chore: add fromMap and toMap methods for Message and WebPlatformException classes

--- packages/worker_method_channel/lib/src/exception.dart
+++ packages/worker_method_channel/lib/src/exception.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 /// Thrown to indicate that a platform interaction failed in the platform
 /// plugin.
 ///
@@ -24,5 +27,23 @@ class WebPlatformException implements Exception {
   @override
   String toString() {
     return 'WebPlatformException(code: $code, message: $message, exception: $exception, stacktrace: $stacktrace)';
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'code': code,
+      'message': message,
+      'exception': exception,
+      'stacktrace': stacktrace.toString(),
+    };
+  }
+
+  factory WebPlatformException.fromMap(Map<String, dynamic> map) {
+    return WebPlatformException(
+      code: map['code'],
+      message: map['message'],
+      exception: map['exception'],
+      stacktrace: map['stacktrace'] == null ? null : StackTrace.fromString(map['stacktrace']),
+    );
   }
 }
--- packages/worker_method_channel/lib/src/message.dart
+++ packages/worker_method_channel/lib/src/message.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 import 'exception.dart';
 
 class Message {
   final int requestId;
   final String method;
+  // primitive types or List, Map
   final Object? body;
   final WebPlatformException? exception;
 
@@ -17,5 +19,24 @@ class Message {
   @override
   String toString() {
     return 'Message(requestId: $requestId, method: $method, body: $body, exception: $exception)';
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    return Message(
+      requestId: map['requestId'] as int,
+      method: map['method'] as String,
+      body: map['body'],
+      exception:
+          map['exception'] == null ? null : WebPlatformException.fromMap(map['exception'] as Map<String, dynamic>),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'requestId': requestId,
+      'method': method,
+      'body': body,
+      'exception': exception?.toMap(),
+    };
   }
 }
