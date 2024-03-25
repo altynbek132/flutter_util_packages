22ce22ea71426c64a7b496c02e636c5cf4e05c99
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-03-25 16:46:41 +0500

    [utils_flutter] chore: move files

--- packages/utils_flutter/lib/utils_flutter.dart
+++ packages/utils_flutter/lib/utils_flutter.dart
@@ -1,3 +1,3 @@
 library utils_flutter;
 
-export 'src/src.dart';
+export 'utils_flutter/src.dart';
--- packages/utils_flutter/lib/utils_flutter/mobx_store_base.dart
+++ packages/utils_flutter/lib/utils_flutter/mobx_store_base.dart
@@ -7,7 +7,7 @@ import 'package:get_it/get_it.dart' hide Disposable;
 import 'package:logger/logger.dart';
 import 'package:mobx/mobx.dart' hide Listenable;
 import 'package:rxdart/rxdart.dart';
-import 'package:utils_dart/utils_dart.dart';
+import 'package:utils_flutter/utils_dart.dart';
 import 'package:utils_flutter/utils_flutter.dart';
 
 abstract class MobxWM<W extends ElementaryWidget> extends WidgetModel<W, Null> with LoggerMixin, DisposableBag {
