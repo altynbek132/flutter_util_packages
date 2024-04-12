1d00182a636b106245b2fa782864759bbb5a26f5
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-04-12 18:37:17 +0500

    [worker_method_channel] test: Add compile test for worker_method_channel package

---
+++ packages/worker_method_channel/test/compile_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import 'package:worker_method_channel/src/src.dart';
+
+Future<void> main() async {
+  // todo: write makefile test that detects compilation fail
+  test('should compile', () async {
+    WebWorkerMethodChannel;
+  });
+}
