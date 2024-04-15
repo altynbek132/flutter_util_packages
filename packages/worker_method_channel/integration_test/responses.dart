3702f0ed80af497fa79575def86ebeac59b92ec1
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-04-15 20:34:57 +0500

    [worker_method_channel] refactor: update responseChecker function to throw exceptions when there are mismatches

--- packages/worker_method_channel/integration_test/responses.dart
+++ packages/worker_method_channel/integration_test/responses.dart
@@ -11,58 +11,39 @@ class Responses {
     ResponseHandler(
       methodName: 'echo',
       response: (request) async => request,
-      responseChecker: (responseOriginal, responseByWorker, expect) {
-        expect(responseOriginal, responseByWorker);
-        return true;
-      },
+      responseChecker: (responseOriginal, responseByWorker, expect) => expect(responseOriginal, responseByWorker),
     ),
     ResponseHandler(
       methodName: 'echoError',
       response: (request) async => throw Exception('echoError'),
-      responseChecker: (responseOriginal, responseByWorker, expect) {
-        expect(responseOriginal, responseByWorker);
-        return true;
-      },
+      responseChecker: (responseOriginal, responseByWorker, expect) => expect(responseOriginal, responseByWorker),
     ),
     ResponseHandler(
       methodName: 'returnMap',
       response: (request) async => {'key': 'value'},
       responseChecker: (responseOriginal, responseByWorker, expect) {
         expect(responseOriginal, responseByWorker);
-        try {
-          (responseByWorker as Map).cast<String, String>();
-          return true;
-        } on Exception catch (_) {
-          return false;
-        }
+        (responseByWorker as Map).cast<String, String>();
       },
     ),
     ResponseHandler(
       methodName: 'returnList',
       response: (request) async => ['value1', 'value2'],
       responseChecker: (responseOriginal, responseByWorker, expect) {
-        try {
-          (responseByWorker as List).cast<String>();
-          return true;
-        } on Exception catch (_) {
-          return false;
-        }
+        (responseByWorker as List).cast<String>();
       },
     ),
     ResponseHandler(
       methodName: 'returnNull',
       response: (request) async => null,
-      responseChecker: (responseOriginal, responseByWorker, expect) {
-        expect(responseOriginal, responseByWorker);
-        return true;
-      },
+      responseChecker: (responseOriginal, responseByWorker, expect) => expect(responseOriginal, responseByWorker),
     ),
     ResponseHandler(
       methodName: 'returnUintList',
       response: (request) async => Uint8List.fromList([1, 2, 3, 4, 5]),
       responseChecker: (responseOriginal, responseByWorker, expect) {
         expect(responseOriginal, responseByWorker);
-        return true;
+        assert(responseByWorker is Uint8List);
       },
     ),
     ResponseHandler(
@@ -78,21 +59,20 @@ class Responses {
         };
       },
       responseChecker: (responseOriginal, responseByWorker, expect) {
-        try {
-          final map = (responseByWorker as Map).castMap();
-          final list = (map['list'] as List).map((e) => (e as Map).castMap()).toList();
-          return list[0]['key2'] is Uint8List;
-        } on Exception catch (_) {
-          return false;
-        }
+        final map = (responseByWorker as Map).castMap();
+        final list = (map['list'] as List).map((e) => (e as Map).castMap()).toList();
+        assert(list[0]['key2'] is Uint8List);
       },
     ),
   ];
 }
 
 /// A function type used to check the response of a method call.
-typedef ResponseChecker = bool Function(
-    Object? responseOriginal, Object? responseByWorker, void Function(dynamic a, dynamic b) expect);
+typedef ResponseChecker<T> = T Function(
+  Object? responseOriginal,
+  Object? responseByWorker,
+  void Function(dynamic a, dynamic b) expect,
+);
 
 /// A helper class for testing responses of method calls.
 class ResponseHandler {
@@ -103,20 +83,26 @@ class ResponseHandler {
   final Future<Object?> Function(Object? request) response;
 
   /// always returns boolean
-  final ResponseChecker responseChecker;
+  final ResponseChecker<bool> responseChecker;
 
   /// Creates a new instance of [ResponseHandler].
   ///
   /// The [methodName] is the name of the method being tested.
   /// The [response] is a function that takes a request object and returns a future response object.
-  /// The [responseChecker] is a function that checks if the original response matches the response received by the worker. It can return Boolean or throw an exception.
+  /// The [responseChecker] is a function that checks if the original response matches the response received by the
+  /// worker. It can should throw an exception.
   ResponseHandler({
     required this.methodName,
     this.requestBody,
     required this.response,
-    required ResponseChecker responseChecker,
-  }) : responseChecker = ((responseOriginal, responseByWorker, expect) => onErrorSync(
-              (() => responseChecker(responseOriginal, responseByWorker, expect)),
-              (error, stackTrace) => false,
-            ));
+    required ResponseChecker<void> responseChecker,
+  }) : responseChecker = ((responseOriginal, responseByWorker, expect) {
+          return onErrorSync(
+            () {
+              responseChecker(responseOriginal, responseByWorker, expect);
+              return true;
+            },
+            (error, stackTrace) => false,
+          );
+        });
 }
