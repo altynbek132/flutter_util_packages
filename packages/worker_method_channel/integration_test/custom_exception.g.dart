7c397a78d0e8d3726a77d30fbece56237ce6747d
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-04-25 12:20:12 +0500

    [worker_method_channel] test: add CustomExceptionFreezed class

---
+++ packages/worker_method_channel/integration_test/custom_exception.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'custom_exception.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CustomExceptionImpl _$$CustomExceptionImplFromJson(
+        Map<String, dynamic> json) =>
+    _$CustomExceptionImpl(
+      message: json['message'] as String,
+    );
+
+Map<String, dynamic> _$$CustomExceptionImplToJson(
+        _$CustomExceptionImpl instance) =>
+    <String, dynamic>{
+      'message': instance.message,
+    };
