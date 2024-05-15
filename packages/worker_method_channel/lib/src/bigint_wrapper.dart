46790c5cfcb78dd7d4752932fe4e4d36b6fe120f
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-05-15 14:49:55 +0500

    [worker_method_channel] feat: add BigIntWrapper class and helper functions
    Added the BigIntWrapper class to handle BigInt values in JSON serialization and deserialization. Also implemented helper functions to recursively wrap and unwrap BigInt values in maps and lists.

---
+++ packages/worker_method_channel/lib/src/bigint_wrapper.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:meta/meta.dart';
+import 'package:utils/utils_dart.dart';
+
+@internal
+class BigIntWrapper {
+  static const key = 'BigIntWrapper';
+  final BigInt value;
+
+  BigIntWrapper(this.value);
+
+  Obj toJson() {
+    return {key: jsonEncode(value.toString())};
+  }
+
+  static BigIntWrapper fromJson(Obj json) {
+    final valueString = jsonDecode(json[key]);
+    return BigIntWrapper(BigInt.parse(valueString));
+  }
+
+  static bool isBigIntWrapper(Obj json) {
+    return json.containsKey(key);
+  }
+}
+
+@internal
+dynamic wrapBigIntRecurse(dynamic obj) {
+  if (obj is BigInt) {
+    return BigIntWrapper(obj).toJson();
+  }
+  if (obj is Map) {
+    return obj.map((key, value) => MapEntry(key, wrapBigIntRecurse(value)));
+  }
+  if (obj is List) {
+    return obj.map((e) => wrapBigIntRecurse(e)).toList();
+  }
+  return obj;
+}
+
+@internal
+dynamic unWrapBigIntRecurse(dynamic obj) {
+  if (obj is Map) {
+    if (BigIntWrapper.isBigIntWrapper(obj.castMap())) {
+      return BigIntWrapper.fromJson(obj.castMap()).value;
+    }
+    return obj.map((key, value) => MapEntry(key, unWrapBigIntRecurse(value)));
+  }
+  if (obj is List) {
+    return obj.map((e) => unWrapBigIntRecurse(e)).toList();
+  }
+  return obj;
+}
