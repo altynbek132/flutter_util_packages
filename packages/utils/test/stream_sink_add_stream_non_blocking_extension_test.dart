1baa9d8f227dcf0bb883d831f0ac53a1c4651a8f
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-05-18 22:13:19 +0500

    [utils] test: add test for stream_sink_add_stream_non_blocking_extension
    Adding a test for the stream_sink_add_stream_non_blocking_extension in the utils package. The test checks if streams are correctly added in a non-blocking manner.

---
+++ packages/utils/test/stream_sink_add_stream_non_blocking_extension_test.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+
+import 'package:test/test.dart';
+import 'package:utils/utils_dart/stream_sink_add_stream_non_blocking_extension.dart';
+
+Future<void> main() async {
+  test('should add stream', () async {
+    final sc = StreamController.broadcast();
+    final sc2 = StreamController()
+      ..addStreamNonBlocking(sc.stream)
+      ..addStreamNonBlocking(sc.stream);
+    sc2.stream.listen(
+      (event) {
+        print('event: $event');
+      },
+      onError: (error) {
+        print('error: $error');
+      },
+      onDone: () {
+        print('done');
+      },
+    );
+    sc.add('Hello');
+    sc.add('World');
+    sc.addError(Exception());
+    sc.add('after error');
+  });
+}
