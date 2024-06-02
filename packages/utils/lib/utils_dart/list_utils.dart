8d200a6b3ae2491a571504998540fe4939f8780b
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-06-02 20:49:23 +0500

    [utls] feat: Add separated extension to Iterable in list_utils.dart

--- packages/utils/lib/utils_dart/list_utils.dart
+++ packages/utils/lib/utils_dart/list_utils.dart
@@ -21,6 +21,18 @@ extension ListInsrtionExtension<T> on List<T> {
   }
 }
 
+extension IterableSeparatedExtension<T> on Iterable<T> {
+  Iterable<T> separated(T separator) sync* {
+    final iterator = this.iterator;
+    if (!iterator.moveNext()) return;
+    yield iterator.current;
+    while (iterator.moveNext()) {
+      yield separator;
+      yield iterator.current;
+    }
+  }
+}
+
 extension ListEdgesExtension<T> on List<T> {
   (V?, V?) edges<V>([V? Function(T? value)? selector]) {
     selector ??= (value) => value as V?;
