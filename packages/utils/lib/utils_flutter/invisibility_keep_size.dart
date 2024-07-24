7b26b0d8f28b3a533f51e488fc22d79431a953b7
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-07-24 19:17:20 +0500

    chore: Add active flag to InvisibilityKeepSize widget

--- packages/utils/lib/utils_flutter/invisibility_keep_size.dart
+++ packages/utils/lib/utils_flutter/invisibility_keep_size.dart
@@ -4,12 +4,17 @@ class InvisibilityKeepSize extends StatelessWidget {
   const InvisibilityKeepSize({
     super.key,
     required this.child,
+    this.active = true,
   });
 
   final Widget child;
+  final bool active;
 
   @override
   Widget build(BuildContext context) {
+    if (!active) {
+      return child;
+    }
     return Visibility(
       visible: false,
       maintainSize: true,
