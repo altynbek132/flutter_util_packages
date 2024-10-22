dc5d7f2f8aa8ce5b9e5aca791e79224dd15c8a11
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2024-10-22 23:07:58 +0500

    refactor(utils_flutter): remove unused logger setup methods streamline disposal process
    - Removed the `logOnStringChange` method from both `MobxWM` and `MobxStoreBase` as was unused.
    - Updated disposal methods to ensure proper completion of disposal without unnecessary initialization.
    - Cleaned up the registration of the object as a singleton until disposal by removing the `GetIt` references.
    - Optimized the logging setup in the `initWidgetModel` method.

--- packages/utils/lib/utils_flutter/mobx_store_base.dart
+++ packages/utils/lib/utils_flutter/mobx_store_base.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:disposing/disposing_dart.dart';
 import 'package:elementary/elementary.dart';
 import 'package:flutter/widgets.dart';
-import 'package:get_it/get_it.dart' hide Disposable;
 import 'package:logger/logger.dart';
 import 'package:mobx/mobx.dart' hide Listenable;
 import 'package:rxdart/rxdart.dart';
@@ -13,43 +12,31 @@ abstract class MobxWM<W extends ElementaryWidget> extends WidgetModel<W, Null> w
   @override
   void initWidgetModel() {
     super.initWidgetModel();
-    setupLoggers();
     logger.i('initWidgetModel');
+    setupLoggers();
   }
 
   @protected
   void setupLoggers() {}
 
   @protected
-  void logOnStringChange(ValueGetter getter) {
-    _logOnStringChange(getter, disposeStream, logger, this);
-  }
-
-  @protected
   void setupObservableLoggers(Iterable<ValueGetter> formattedValueGetters, Logger log) {
     _setupObservableLoggers(formattedValueGetters, log, this);
   }
 
-  @protected
-  void registerAsSingletonUntilDispose<T extends MobxWM>() {
-    GetIt.I.registerSingleton<T>(this as T);
-    SyncCallbackDisposable(() => GetIt.I.unregister<T>(instance: this)).disposeOn(this);
-  }
-
-  Future<void>? get disposeFuture => _disposeCompleter?.future;
-  Completer<void>? _disposeCompleter;
+  Future<void> get disposeFuture => _disposeCompleter.future;
+  final Completer<void> _disposeCompleter = Completer<void>();
 
   @override
   void dispose() {
     () async {
-      _disposeCompleter = Completer<void>();
       logger.i('dispose init');
       _disposeStreamC.add(null);
       _disposeStreamC.close();
       super.dispose();
       await super.disposeAsync();
+      _disposeCompleter.complete();
       logger.i('dispose finish');
-      _disposeCompleter?.complete();
     }();
   }
 
@@ -59,20 +46,8 @@ abstract class MobxWM<W extends ElementaryWidget> extends WidgetModel<W, Null> w
 
 abstract class MobxStoreBase with LoggerMixin, DisposableBag {
   @protected
-  void logOnStringChange(ValueGetter getter) {
-    _logOnStringChange(getter, disposeStream, logger, this);
-  }
-
-  @protected
   void setupObservableLoggers(Iterable<ValueGetter> formattedValueGetters, Logger log) {
     _setupObservableLoggers(formattedValueGetters, log, this);
-  }
-
-  @protected
-  void registerAsSingletonUntilDispose<T extends MobxStoreBase>() {
-    final locator = GetIt.instance;
-    locator.registerSingleton<T>(this as T);
-    SyncCallbackDisposable(() => locator.unregister<T>(instance: this)).disposeOn(this);
   }
 
   @override
@@ -80,26 +55,16 @@ abstract class MobxStoreBase with LoggerMixin, DisposableBag {
     logger.i('dispose init');
     _disposeStreamC.add(null);
     _disposeStreamC.close();
-    logger.i('dispose finish');
     await super.disposeAsync();
+    logger.i('dispose finish');
+    _disposeCompleter.complete();
   }
+
+  Future<void> get disposeFuture => _disposeCompleter.future;
+  final Completer<void> _disposeCompleter = Completer<void>();
 
   late final disposeStream = _disposeStreamC.stream.shareReplay(maxSize: 1);
   final _disposeStreamC = StreamController<void>();
-}
-
-void _logOnStringChange(
-  ValueGetter<dynamic> getter,
-  ReplayStream<void> disposeStream,
-  Logger logger,
-  DisposableBag bag,
-) {
-  MobxUtilsDart.fromGetter(getter)
-      .takeUntil(disposeStream)
-      .map((event) => event.toString())
-      .distinct()
-      .listen((event) => logger.i('toString change: ${event}'))
-      .disposeOn(bag);
 }
 
 void _setupObservableLoggers(Iterable<ValueGetter<dynamic>> formattedValueGetters, Logger log, DisposableBag bag) {
