0eddbb593c9d89086b3720c1c286bfc017f3485e
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2025-03-06 16:34:51 +0500

    feat(utils): add MobxMVController and CounterScreenWidget for state management demo
    This commit introduces a new MobxMVController and a CounterScreenWidget that demonstrates state management using MobX. The CounterScreenWidget allows users to increment a counter and observe changes in the UI.

--- packages/utils/example/lib/main.dart
+++ packages/utils/example/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
+import 'package:utils/utils_flutter.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,67 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return const MaterialApp(home: CounterScreenWidget());
+  }
+}
+
+class DemoController extends MobxMVController {
+  final counter = Observable(0);
+
+  void increment() {
+    runInAction(() {
+      counter.value++;
+    });
+  }
+
+  @override
+  Future<void> init() async {
+    await super.init();
+    // ignore: avoid_print
+    print('init');
+  }
+
+  @override
+  Future<void> disposeAsync() async {
+    // ignore: avoid_print
+    print('dispose');
+    await super.disposeAsync();
+  }
+}
+
+class CounterScreenWidget extends StatefulWidget {
+  const CounterScreenWidget({super.key});
+
+  @override
+  State<CounterScreenWidget> createState() => _CounterScreenWidgetState();
+}
+
+class _CounterScreenWidgetState extends State<CounterScreenWidget>
+    with MobxMVControllerMixin<DemoController, CounterScreenWidget> {
+  @override
+  DemoController controllerFactory() => DemoController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (context) {
+        return Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('You have pushed the button this many times:'),
+                Text('${c.counter.value}', style: Theme.of(context).textTheme.headlineMedium),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: c.increment,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 }
