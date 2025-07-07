2f8159e190c3ae6185db461d9ed121798a53ac8f
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2025-07-07 18:42:51 +0500

    feat(utils): add additional exports for rx_db, safe_area_only, and size_builder

--- packages/utils/lib/utils_flutter/utils_flutter.dart
+++ packages/utils/lib/utils_flutter/utils_flutter.dart
@@ -7,5 +7,8 @@ export 'hex_color.dart';
 export 'invisibility_keep_size.dart';
 export 'mobx_store_base.dart';
 export 'mobx_utils.dart';
+export 'rx_db.dart';
+export 'safe_area_only.dart';
+export 'size_builder.dart';
 export 'size_reporting_widget.dart';
 export 'text_style_height_extension.dart';
