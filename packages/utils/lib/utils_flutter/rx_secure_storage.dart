d328f8d083b1a6e16136b8fb1a56e070575ba326
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2025-07-17 23:22:45 +0500

    refactor(rx_secure_storage): restructure RxSecureStorage methods for improved clarity and consistency

--- packages/utils/lib/utils_flutter/rx_secure_storage.dart
+++ packages/utils/lib/utils_flutter/rx_secure_storage.dart
@@ -1,29 +1,175 @@
 import 'dart:async';
+import 'package:flutter/src/foundation/basic_types.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:rxdart/rxdart.dart';
 
 class RxSecureStorage {
-  final FlutterSecureStorage storage;
+  final FlutterSecureStorage _storage;
 
-  RxSecureStorage({required this.storage});
+  RxSecureStorage({required FlutterSecureStorage storage}) : _storage = storage;
 
-  final streams = <String, BehaviorSubject<String?>>{};
+  final _streams = <String, BehaviorSubject<String?>>{};
+
+  AndroidOptions get aOptions => _storage.aOptions;
+
+  Future<bool> containsKey({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) {
+    return _storage.containsKey(
+      key: key,
+      iOptions: iOptions,
+      aOptions: aOptions,
+      lOptions: lOptions,
+      webOptions: webOptions,
+      mOptions: mOptions,
+      wOptions: wOptions,
+    );
+  }
+
+  Future<void> delete({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    await _storage.delete(
+      key: key,
+      iOptions: iOptions,
+      aOptions: aOptions,
+      lOptions: lOptions,
+      webOptions: webOptions,
+      mOptions: mOptions,
+      wOptions: wOptions,
+    );
+    _streams[key]?.add(null);
+  }
+
+  Future<void> deleteAll({
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) {
+    return _storage.deleteAll(
+      iOptions: iOptions,
+      aOptions: aOptions,
+      lOptions: lOptions,
+      webOptions: webOptions,
+      mOptions: mOptions,
+      wOptions: wOptions,
+    );
+  }
+
+  IOSOptions get iOptions => _storage.iOptions;
+
+  Future<bool?> isCupertinoProtectedDataAvailable() {
+    return _storage.isCupertinoProtectedDataAvailable();
+  }
+
+  LinuxOptions get lOptions => _storage.lOptions;
+
+  MacOsOptions get mOptions => _storage.mOptions;
+
+  Stream<bool>? get onCupertinoProtectedDataAvailabilityChanged =>
+      _storage.onCupertinoProtectedDataAvailabilityChanged;
+
+  Future<String?> read({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) {
+    return _storage.read(
+      key: key,
+      iOptions: iOptions,
+      aOptions: aOptions,
+      lOptions: lOptions,
+      webOptions: webOptions,
+      mOptions: mOptions,
+      wOptions: wOptions,
+    );
+  }
+
+  Stream<String?> readStream({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) {
+    if (_streams[key] == null) {
+      _streams[key] = BehaviorSubject<String?>();
+      read(
+        key: key,
+        iOptions: iOptions,
+        aOptions: aOptions,
+        lOptions: lOptions,
+        webOptions: webOptions,
+        mOptions: mOptions,
+        wOptions: wOptions,
+      ).then((value) => _streams[key]!.add(value));
+    }
+    return _streams[key]!.stream;
+  }
+
+  Future<Map<String, String>> readAll({
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) {
+    return _storage.readAll(
+      iOptions: iOptions,
+      aOptions: aOptions,
+      lOptions: lOptions,
+      webOptions: webOptions,
+      mOptions: mOptions,
+      wOptions: wOptions,
+    );
+  }
+
+  WindowsOptions get wOptions => _storage.wOptions;
+
+  WebOptions get webOptions => _storage.webOptions;
 
   Future<void> write({
     required String key,
-    required String value,
+    required String? value,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
   }) async {
-    await storage.write(key: key, value: value);
-    streams[key]?.add(value);
-  }
-
-  Stream<String?> read({
-    required String key,
-  }) {
-    if (streams[key] == null) {
-      streams[key] = BehaviorSubject<String?>();
-      storage.read(key: key).then((value) => streams[key]!.add(value));
-    }
-    return streams[key]!.stream;
+    await _storage.write(
+      key: key,
+      value: value,
+      iOptions: iOptions,
+      aOptions: aOptions,
+      lOptions: lOptions,
+      webOptions: webOptions,
+      mOptions: mOptions,
+      wOptions: wOptions,
+    );
+    _streams[key]?.add(value);
   }
 }
