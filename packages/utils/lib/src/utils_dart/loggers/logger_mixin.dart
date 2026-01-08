1dfb55b94a527c663503b6379c0725ea4ef3caee
Altynbek Aidarbekov <altynbek.ktl@gmail.com>
2026-01-08 15:03:33 +0500

    refactor(logger_mixin): replace Logger with PrefixedL for enhanced logging structure

--- packages/utils/lib/src/utils_dart/loggers/logger_mixin.dart
+++ packages/utils/lib/src/utils_dart/loggers/logger_mixin.dart
@@ -1,8 +1,6 @@
-import 'package:logger/logger.dart';
-import 'dart:developer';
+import 'package:l/l.dart';
 
-import 'package:dio/dio.dart';
-import 'package:platform_info/platform_info.dart';
+import 'package:utils/utils_dart.dart';
 
 /// A mixin that provides a logger instance.
 ///
@@ -10,229 +8,85 @@ import 'package:platform_info/platform_info.dart';
 /// It uses the `getLogger` function from the `logger` package to create the logger.
 mixin LoggerMixin {
   static var useRuntimeType = true;
-  Logger get logger => getLogger(useRuntimeType ? runtimeType.toString() : 'LoggerMixin');
+  LNullable get logger => PrefixedL(useRuntimeType ? runtimeType.toString() : 'LoggerMixin');
 }
 
-/// A global logger instance.
-final loggerGlobal = getLogger('GLOBAL');
+final class PrefixedL extends LNullable {
+  final String prefix;
 
-var _kLoggerShouldLog = true;
-
-/// Sets the value of [loggerShouldLog] to control whether the logger should log or not.
-///
-/// The [loggerShouldLog] parameter determines whether the logger should log or not.
-/// If set to `true`, the logger will log messages. If set to `false`, the logger will not log messages.
-void setLoggerShouldLog(bool loggerShouldLog) {
-  _kLoggerShouldLog = loggerShouldLog;
-}
-
-/// Logs function in which this function is called.
-///
-/// The [print] function is used to output the log message.
-/// The [message] parameter is an optional message to be logged.
-/// The [depth] parameter specifies the depth of the stack trace to be included in the log.
-/// The [full] parameter specifies whether the full stack trace should be included in the log.
-void logFunction(
-  void Function(Object message) print, {
-  Object? message,
-  int? depth,
-  bool? full,
-}) {
-  if (!_kLoggerShouldLog) return;
-  final str = () {
-    if (full ?? false) {
-      return StackTrace.current.toString();
-    }
-    return StackTrace.current
-        .toString()
-        .split('\n')[depth ?? 2]
-        .split(' ')
-        .map((e) => e.trim())
-        .where((element) => element.isNotEmpty)
-        .skip(1)
-        .join(' ');
-  }();
-  print('${message ?? ''}: trace: ${str}');
-}
-
-/// Returns a logger instance with the specified configuration.
-///
-/// The [className] parameter specifies the name of the class that will be associated with the logger.
-/// The [printCallingFunctionName] parameter determines whether to print the calling function name in log messages.
-/// The [printCallstack] parameter determines whether to print the call stack in log messages.
-/// The [exludeLogsFromClasses] parameter is a list of class names for which log messages should be excluded.
-/// The [showOnlyClass] parameter specifies a class name to filter log messages and only show logs from that class.
-///
-/// The returned logger instance is configured with a [LogAllTheTimeFilter] filter, a [SimpleLogPrinter] printer,
-/// and a [MultipleLoggerOutput] output with a [DevLogOutput].
-Logger getLogger(
-  String className, {
-  bool printCallingFunctionName = true,
-  bool printCallstack = false,
-  List<String> exludeLogsFromClasses = const [],
-  String? showOnlyClass,
-}) {
-  return Logger(
-    filter: LogAllTheTimeFilter(),
-    printer: SimpleLogPrinter(
-      className,
-      printCallingFunctionName: printCallingFunctionName,
-      printCallStack: printCallstack,
-      showOnlyClass: showOnlyClass,
-      exludeLogsFromClasses: exludeLogsFromClasses,
-    ),
-    output: MultipleLoggerOutput([
-      DevLogOutput(),
-    ]),
-  );
-}
-
-var kUseAlwaysPrint = false;
-
-/// A simple log printer that implements the [LogPrinter] interface.
-class SimpleLogPrinter extends LogPrinter {
-  final String className;
-  final bool printCallingFunctionName;
-  final bool printCallStack;
-  final List<String> exludeLogsFromClasses;
-  final String? showOnlyClass;
-
-  /// Creates a new instance of [SimpleLogPrinter].
-  ///
-  /// The [className] parameter specifies the name of the class that is using the logger.
-  /// The [printCallingFunctionName] parameter determines whether to print the calling function name in the log.
-  /// The [printCallStack] parameter determines whether to print the call stack in the log.
-  /// The [exludeLogsFromClasses] parameter is a list of class names to exclude from logging.
-  /// The [showOnlyClass] parameter specifies a class name to show logs only for that class.
-  SimpleLogPrinter(
-    this.className, {
-    this.printCallingFunctionName = true,
-    this.printCallStack = false,
-    this.exludeLogsFromClasses = const [],
-    this.showOnlyClass,
-  });
-
-  String _labelFor(Level level) {
-    final prefix = SimplePrinter.levelPrefixes[level];
-    final color = SimplePrinter.levelColors[level];
-
-    if (prefix == null) {
-      return '';
-    }
-    if (color == null) {
-      return prefix;
-    }
-    return color(prefix);
+  factory PrefixedL(String prefix) {
+    return PrefixedL.internal(prefix);
   }
 
-  /// Returns a list of log messages based on the provided [event].
-  ///
-  /// The [event] parameter represents a log event containing information such as the log level, message, error, and stack trace.
-  /// This method formats the log message with a timestamp, class name, and optional stack trace.
-  /// It also filters out logs based on exclusion and inclusion criteria.
-  ///
-  /// If the log message belongs to an excluded class or does not match the specified class for inclusion, an empty list is returned.
-  ///
-  /// The returned list contains each line of the formatted log message, followed by the error and stack trace (if available).
-  ///
-  /// Example usage:
-  /// ```dart
-  /// final logger = SimpleLogPrinter();
-  /// final logEvent = LogEvent(
-  ///   level: LogLevel.info,
-  ///   message: 'This is an info message',
-  ///   error: null,
-  ///   stackTrace: null,
-  /// );
-  /// final logMessages = logger.log(logEvent);
-  /// print(logMessages);
-  /// ```
+  PrefixedL.internal(this.prefix) : super.internal();
+
+  /// A shout [message] is always displayed
   @override
-  List<String> log(LogEvent event) {
-    final now = DateTime.now();
-    final prefix =
-        '${_labelFor(event.level)} [${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')}]';
+  void s(Object? message, [LogMessageContext? context]) => super.s('$prefix $message', context);
 
-    final methodNameSection = printCallingFunctionName;
-    final stackLog = event.stackTrace.toString();
-    final output =
-        '$prefix ${methodNameSection ? className : ''} - ${event.message}${printCallStack ? '\nSTACKTRACE:\n$stackLog' : ''}';
-
-    if (exludeLogsFromClasses.any((excludeClass) => className == excludeClass) ||
-        (showOnlyClass != null && className != showOnlyClass)) return [];
-
-    final List<String> result = [];
-
-    for (var line in output.split('\n')) {
-      result.add(line);
-    }
-
-    final err = event.error;
-    result.addAll([
-      if (event.error != null) ...[
-        'error:',
-        if (err is DioException) err.requestOptions.uri.toString(),
-        err.toString(),
-      ],
-      if (event.stackTrace != null) ...[
-        'stackTrace:',
-        event.stackTrace.toString(),
-      ],
-    ]);
-
-    return result;
-  }
-}
-
-class MultipleLoggerOutput extends LogOutput {
-  final List<LogOutput> logOutputs;
-
-  MultipleLoggerOutput(this.logOutputs);
-
+  /// Regular [message] with verbose level 1
   @override
-  void output(OutputEvent event) {
-    for (var logOutput in logOutputs) {
-      try {
-        logOutput.output(event);
-      } catch (e) {
-        log('Log output failed');
-      }
-    }
-  }
-}
+  void v(Object? message, [LogMessageContext? context]) => super.v('$prefix $message', context);
 
-class LogAllTheTimeFilter extends LogFilter {
+  /// Regular [message] with verbose level 1
   @override
-  bool shouldLog(LogEvent event) {
-    return true;
-  }
-}
+  void v1(Object? message, [LogMessageContext? context]) => super.v1('$prefix $message', context);
 
-class DevLogOutput extends LogOutput {
+  /// Regular [message] with verbose level 2
   @override
-  void output(OutputEvent event) {
-    if (!_kLoggerShouldLog) {
-      return;
-    }
-    if (kUseAlwaysPrint) {
-      // ignore: avoid_print
-      event.lines.forEach(print);
-      return;
-    }
-    // ignore: avoid_print
-    var logFunction = (String message) => print(message);
-    if (_isIO) {
-      logFunction = (String message) => log(message);
-    }
-    event.lines.forEach(logFunction);
-  }
+  void vv(Object? message, [LogMessageContext? context]) => super.vv('$prefix $message', context);
 
-  // ! workaround for dart compile js imported with [Worker] class
-  bool get _isIO {
-    try {
-      return Platform.I.vm;
-    } catch (e) {
-      return false;
-    }
-  }
+  /// Regular [message] with verbose level 2
+  @override
+  void v2(Object? message, [LogMessageContext? context]) => super.v2('$prefix $message', context);
+
+  /// Regular [message] with verbose level 3
+  @override
+  void vvv(Object? message, [LogMessageContext? context]) => super.vvv('$prefix $message', context);
+
+  /// Regular [message] with verbose level 3
+  @override
+  void v3(Object? message, [LogMessageContext? context]) => super.v3('$prefix $message', context);
+
+  /// Regular [message] with verbose level 4
+  @override
+  void vvvv(Object? message, [LogMessageContext? context]) => super.vvvv('$prefix $message', context);
+
+  /// Regular [message] with verbose level 4
+  @override
+  void v4(Object? message, [LogMessageContext? context]) => super.v4('$prefix $message', context);
+
+  /// Regular [message] with verbose level 5
+  @override
+  void vvvvv(Object? message, [LogMessageContext? context]) => super.vvvvv('$prefix $message', context);
+
+  /// Regular [message] with verbose level 5
+  @override
+  void v5(Object? message, [LogMessageContext? context]) => super.v5('$prefix $message', context);
+
+  /// Regular [message] with verbose level 6
+  @override
+  void vvvvvv(Object? message, [LogMessageContext? context]) => super.vvvvvv('$prefix $message', context);
+
+  /// Regular [message] with verbose level 6
+  @override
+  void v6(Object? message, [LogMessageContext? context]) => super.v6('$prefix $message', context);
+
+  /// Inform [message] with verbose level 3
+  @override
+  void i(Object? message, [LogMessageContext? context]) => super.i('$prefix $message', context);
+
+  /// Warning [message] with verbose level 2
+  @override
+  void w(Object? message, [StackTrace? stackTrace, LogMessageContext? context]) =>
+      super.w('$prefix $message', stackTrace, context);
+
+  /// Error [message] with verbose level 1
+  @override
+  void e(Object? message, [StackTrace? stackTrace, LogMessageContext? context]) =>
+      super.e('$prefix $message', stackTrace, context);
+
+  /// Debug [message] with verbose level 4
+  @override
+  void d(Object? message, [LogMessageContext? context]) => super.d('$prefix $message', context);
 }
